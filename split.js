dc9b5d6ec0a9f3f1e0b399d0a971edac428f23c9
demzou <clemence.debaig@gmail.com>
2020-11-11 15:32:02 +0000

    3 steps recording + styling

--- split.js
+++ split.js
@@ -1,11 +1,30 @@
 let preview = document.getElementById("preview");
 let recording = document.getElementById("recording");
+let pageTitle = document.getElementById("title");
 let startButton = document.getElementById("startButton");
 let stopButton = document.getElementById("stopButton");
 let downloadButton = document.getElementById("downloadButton");
 let uploadButton = document.getElementById("uploadButton");
 let logElement = document.getElementById("log");
 let file = 0;
+
+
+// ----- Hide elements when page loads (step2)
+recording.style.display = 'none';
+uploadButton.style.display = 'none';
+stopButton.style.display = 'none';
+
+// ---- Show preview when land on page
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: false
+  }).then(stream => {
+    preview.srcObject = stream;
+    //downloadButton.href = stream;
+    preview.captureStream = preview.captureStream || preview.mozCaptureStream;
+    return new Promise(resolve => preview.onplaying = resolve);
+  })
+
 
 // ----- Recording
 
@@ -23,6 +42,13 @@ function wait(delayInMS) {
 
 // Recording function
 function startRecording(stream, lengthInMS) {
+
+    // Update info on page
+    startButton.innerHTML = "Recording...";
+    startButton.style.backgroundColor = "red";
+    startButton.style.color = "white";
+    pageTitle.innerHTML = "Recording";
+
   let recorder = new MediaRecorder(stream);
   let data = [];
  
@@ -53,21 +79,33 @@ function stop(stream) {
 
 // Start + Recording + download
 startButton.addEventListener("click", function() {
-  navigator.mediaDevices.getUserMedia({
-    video: true,
-    audio: true
-  }).then(stream => {
-    preview.srcObject = stream;
-    downloadButton.href = stream;
-    preview.captureStream = preview.captureStream || preview.mozCaptureStream;
-    return new Promise(resolve => preview.onplaying = resolve);
-  }).then(() => startRecording(preview.captureStream(), recordingTimeMS))
+//   navigator.mediaDevices.getUserMedia({
+//     video: true,
+//     audio: false
+  //})
+  //.then(stream => {
+//     preview.srcObject = stream;
+    //downloadButton.href = stream;
+    // preview.captureStream = preview.captureStream || preview.mozCaptureStream;
+    // return new Promise(resolve => preview.onplaying = resolve);
+  //}).then(
+      //() => 
+      startRecording(preview.captureStream(), recordingTimeMS)
+      //)
   .then (recordedChunks => {
+
+    //Update elements on page
+    preview.style.display = 'none';
+    startButton.style.display = 'none';
+    recording.style.display='';
+    uploadButton.style.display = '';
+    pageTitle.innerHTML = "Replay & Upload";
+    //downloadButton.style.display='';
     let recordedBlob = new Blob(recordedChunks, { type: "video/webm" });
     file = recordedBlob;
     recording.src = URL.createObjectURL(recordedBlob);
-    downloadButton.href = recording.src;
-    downloadButton.download = "RecordedVideo.webm";             // Match file name with the one below (create global variable)
+    //downloadButton.href = recording.src;
+    //downloadButton.download = "RecordedVideo.webm";             // Match file name with the one below (create global variable)
     
     log("Successfully recorded " + recordedBlob.size + " bytes of " +
         recordedBlob.type + " media.");
@@ -109,32 +147,32 @@ function run(obj) {
 
 //library
 function resumableUpload(e) {
-document.getElementById("progress").innerHTML = "Initializing.";
-const f = e.target;
-const resource = {
-    fileName: f.fileName,
-    fileSize: f.fileSize,
-    fileType: f.fileType,
-    fileBuffer: f.result,
-    accessToken: accessToken,
-    folderId: '1UBJ-nCLMRzh16SdjQCXzAPODaTxQZNWl'
-};
-const ru = new ResumableUploadToGoogleDrive();
-ru.Do(resource, function(res, err) {
-    if (err) {
-    console.log(err);
-    return;
-    }
-    console.log(res);
-    let msg = "";
-    if (res.status == "Uploading") {
-    msg =
-        Math.round(
-        (res.progressNumber.current / res.progressNumber.end) * 100
-        ) + "%";
-    } else {
-    msg = res.status;
-    }
-    document.getElementById("progress").innerText = msg;
-});
+    document.getElementById("progress").innerHTML = "Initializing.";
+    const f = e.target;
+    const resource = {
+        fileName: f.fileName,
+        fileSize: f.fileSize,
+        fileType: f.fileType,
+        fileBuffer: f.result,
+        accessToken: accessToken,
+        folderId: '1UBJ-nCLMRzh16SdjQCXzAPODaTxQZNWl'
+    };
+    const ru = new ResumableUploadToGoogleDrive();
+    ru.Do(resource, function(res, err) {
+        if (err) {
+        console.log(err);
+        return;
+        }
+        console.log(res);
+        let msg = "";
+        if (res.status == "Uploading") {
+        msg =
+            Math.round(
+            (res.progressNumber.current / res.progressNumber.end) * 100
+            ) + "%";
+        } else {
+        msg = res.status;
+        }
+        document.getElementById("progress").innerText = msg;
+    });
 }
