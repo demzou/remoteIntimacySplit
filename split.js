5156ab2f061a947a0d53d1f6bf95ce046169ef98
demzou <clemence.debaig@gmail.com>
2020-12-02 13:31:48 +0000

    Test with setting up gapi and retrieving token automatically

--- split.js
+++ split.js
@@ -1,5 +1,150 @@
+var d = new Date();
+d.setTime(d.getTime() + (30*24*60*60*1000)); //keep cookie 30 days
+var expires = "expires=" + d.toGMTString();         
+document.cookie = "visitName" + "=Hailin;" + expires + ";path=/;SameSite=None;Secure"; //can set SameSite=Lax also
 
-const accessToken = 'ya29.A0AfH6SMB1Hwe2tZyiaVbPOdQ4Ix5_BgxYTWd4AA6uOC17qkUlnqwCXmtd2WD-E5qyciLMntn9JJqd9Exmi1Zc5i9YjtGU1zz0A5FazYr3ZIVF4Nt0He__y3dXoqQWWbjV3CKhKil8x09j5BUf6BXvj91Vw1BkX1mcS9__vlEmoI8';
+
+
+//const accessToken = 'ya29.A0AfH6SMB1Hwe2tZyiaVbPOdQ4Ix5_BgxYTWd4AA6uOC17qkUlnqwCXmtd2WD-E5qyciLMntn9JJqd9Exmi1Zc5i9YjtGU1zz0A5FazYr3ZIVF4Nt0He__y3dXoqQWWbjV3CKhKil8x09j5BUf6BXvj91Vw1BkX1mcS9__vlEmoI8';
+
+//----- GOOGLE API SIGN IN
+//https://developers.google.com/drive/api/v3/quickstart/js
+
+
+// function start() {
+//     // 2. Initialize the JavaScript client library.
+//     gapi.client.init({
+//       'apiKey': 'AIzaSyBaEbFQh_smzM8eu0AhejSy9DilRm3Bl6o',
+//       // clientId and scope are optional if auth is not required.
+//       'clientId': '1010352766136-afp7r1mhlmfh4eutea2lta4s8treeotg.apps.googleusercontent.com',
+//       'scope': 'https://www.googleapis.com/auth/drive',
+//     }).then(function() {
+//       // 3. Initialize and make the API request.
+//       return gapi.client.request({
+//         'path': 'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest',
+//       })
+//     }).then(function(response) {
+//       console.log(response.result);
+//     }, function(reason) {
+//       console.log('Error: ' + reason.result.error.message);
+//     });
+//   };
+//   // 1. Load the JavaScript client library.
+//   gapi.load('client', start);
+
+
+// Client ID and API key from the Developer Console
+var CLIENT_ID = '1010352766136-afp7r1mhlmfh4eutea2lta4s8treeotg.apps.googleusercontent.com';
+var API_KEY = 'AIzaSyBaEbFQh_smzM8eu0AhejSy9DilRm3Bl6o';
+
+// Array of API discovery doc URLs for APIs used by the quickstart
+var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+//var SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly';
+var SCOPES = 'https://www.googleapis.com/auth/drive';
+
+// var authorizeButton = document.getElementById('authorize_button');
+// var signoutButton = document.getElementById('signout_button');
+
+/**
+ *  On load, called to load the auth2 library and API client library.
+ */
+function handleClientLoad() {
+  gapi.load('client:auth2', initClient);
+}
+
+/**
+ *  Initializes the API client library and sets up sign-in state
+ *  listeners.
+ */
+function initClient() {
+  gapi.client.init({
+    apiKey: API_KEY,
+    clientId: CLIENT_ID,
+    discoveryDocs: DISCOVERY_DOCS,
+    scope: SCOPES
+  })
+//   .then(function () {
+//     // Listen for sign-in state changes.
+//     gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+//     // Handle the initial sign-in state.
+//     updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+//     // authorizeButton.onclick = handleAuthClick;
+//     // signoutButton.onclick = handleSignoutClick;
+ // }
+//   , function(error) {
+//     //appendPre(JSON.stringify(error, null, 2));
+//   });
+}
+
+/**
+ *  Called when the signed in status changes, to update the UI
+ *  appropriately. After a sign-in, the API is called.
+ */
+// function updateSigninStatus(isSignedIn) {
+//   if (isSignedIn) {
+//     authorizeButton.style.display = 'none';
+//     signoutButton.style.display = 'block';
+//     listFiles();
+//   } else {
+//     authorizeButton.style.display = 'block';
+//     signoutButton.style.display = 'none';
+//   }
+// }
+
+/**
+ *  Sign in the user upon button click.
+ */
+// function handleAuthClick(event) {
+//   gapi.auth2.getAuthInstance().signIn();
+// }
+
+/**
+ *  Sign out the user upon button click.
+ */
+// function handleSignoutClick(event) {
+//   gapi.auth2.getAuthInstance().signOut();
+// }
+
+// /**
+//  * Append a pre element to the body containing the given message
+//  * as its text node. Used to display the results of the API call.
+//  *
+//  * @param {string} message Text to be placed in pre element.
+//  */
+// function appendPre(message) {
+//   var pre = document.getElementById('content');
+//   var textContent = document.createTextNode(message + '\n');
+//   pre.appendChild(textContent);
+// }
+
+// /**
+//  * Print files.
+//  */
+// function listFiles() {
+//   gapi.client.drive.files.list({
+//     'pageSize': 10,
+//     'fields': "nextPageToken, files(id, name)"
+//   }).then(function(response) {
+//     appendPre('Files:');
+//     var files = response.result.files;
+//     if (files && files.length > 0) {
+//       for (var i = 0; i < files.length; i++) {
+//         var file = files[i];
+//         appendPre(file.name + ' (' + file.id + ')');
+//       }
+//     } else {
+//       appendPre('No files found.');
+//     }
+//   });
+// }
+
+
+
+//------------------------------
 
 let nav = document.querySelector('header');
 let content = document.getElementById('mainContainer');
@@ -287,6 +432,16 @@ stopButton.addEventListener("click", function() {
 
 
 // ---- Upload
+
+let accessToken;
+
+// dirty timeout function to wait for credentials to be fetched
+// need rewritting with promises!!!
+setTimeout(function(){ 
+    accessToken = gapi.auth.getToken().access_token; // Please set access token here.
+}, 20000);
+
+
 
 // GDrive API Access token
 //const accessToken = gapi.auth.getToken().access_token; // Please set access token here.
